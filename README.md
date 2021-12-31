1081e494612f1433de9e274e44d5ae4d9d78809a
BjoernLuig <bjoernluig@gmail.com>
2021-12-31 15:35:05 +0100

    addedfritzing diagramm

--- README.md
+++ README.md
@@ -1,16 +1,22 @@
 # Schrödingers Dice
 
-A divice, which generates **real** random numbers using the time between the **radioactive** decays of americanium.
-This Projct is highly inspired by [The muon-powered, universe-bifurcating, random number machine](https://www.youtube.com/watch?v=gwIGnATzBTg&list=FLIN6YKhyzmC_FIHTg9GV0EA&index=13&t=27s) by [AlphaPhoenix](https://www.youtube.com/channel/UCCWeRTgd79JL0ilH0ZywSJA)
+A divice, which generates **real** random numbers using the timestemps of **radioactive** decays of americanium.
+This Projct is highly inspired by [The muon-powered, universe-bifurcating, random number machine](https://www.youtube.com/watch?v=gwIGnATzBTg&list=FLIN6YKhyzmC_FIHTg9GV0EA&index=13&t=27s) by [AlphaPhoenix](https://www.youtube.com/channel/UCCWeRTgd79JL0ilH0ZywSJA).
 
-![image](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/activity.jpg)
 
-![image](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/americanium.jpg)
 
-![image](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/overview.jpg)
+![activity](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/activity.jpg)
 
-![image](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/random-selection.jpg)
 
-![image](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/random.jpg)
 
-![image]https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/voltage.jpg)
+![circuit](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/circuit.jpg)
+
+![americanium](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/americanium.jpg)
+
+![overview](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/overview.jpg)
+
+![random-selection](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/random-selection.jpg)
+
+![random](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/random.jpg)
+
+![voltage](https://github.com/BjoernLuig/schroedingers-dice/blob/main/imgs/voltage.jpg)
--- schroedingers-dice.ino
+++ schroedingers-dice.ino
@@ -1,6 +1,5 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
 
 // pins
 #define GEIGER_COUNTER 2
@@ -11,111 +10,97 @@
 #define SIGNAL_LAMP 11
 #define VOLTAGE A2
 
-
 // settings
-#define COUNTING_INTERVAL 30000 // 30 Seconds
-#define CONVERSION_INDEX 151 // counts per minute /(micro Sv/h)
-#define BACKGROUND 25 // backround counts per minutes
-#define VOLTAGE_SCALE 0.00851 //  0.00955
-#define MICROS_FAKTOR 4 // resolution for arduino nano
+#define COUNTING_INTERVAL 30000 // counting every 30 seconds
+#define CONVERSION_INDEX 151    // (counts per minute)/(micro Sv/h)
+#define BACKGROUND 25           // backround counts per minutes
+#define VOLTAGE_SCALE 0.00851   // (battery voltage)/(sensor voltage) (might not be very accurate)
+#define MICROS_FAKTOR 4         // micros() counts in steps of 4 on an arduino nano
 
-
-// LCD
+// LCD display
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-byte radioactiveUpperLeft[] = { // see to generate the bytecode https://maxpromer.github.io/LCD-Character-Creator/
-  B00000,
-  B00100,
-  B01110,
-  B01110,
-  B11111,
-  B11111,
-  B00000,
-  B00000
-};
+byte radioactiveUpperLeft[] = { // see https://maxpromer.github.io/LCD-Character-Creator/ to generate the bytecode
+    B00000,
+    B00100,
+    B01110,
+    B01110,
+    B11111,
+    B11111,
+    B00000,
+    B00000};
 byte radioactiveUpperCenter[] = {
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00100,
-  B01110,
-  B00100
-};
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00100,
+    B01110,
+    B00100};
 byte radioactiveUpperRight[] = {
-  B00000,
-  B00100,
-  B01110,
-  B01110,
-  B11111,
-  B11111,
-  B00000,
-  B00000
-};
+    B00000,
+    B00100,
+    B01110,
+    B01110,
+    B11111,
+    B11111,
+    B00000,
+    B00000};
 byte radioactiveLowerCenter[] = {
-  B00100,
-  B01110,
-  B01110,
-  B11111,
-  B11111,
-  B00100,
-  B00000,
-  B00000
-};
+    B00100,
+    B01110,
+    B01110,
+    B11111,
+    B11111,
+    B00100,
+    B00000,
+    B00000};
 byte batteryLeft[] = {
-  B00011,
-  B00010,
-  B00010,
-  B00010,
-  B00010,
-  B00010,
-  B00010,
-  B00011
-};
+    B00011,
+    B00010,
+    B00010,
+    B00010,
+    B00010,
+    B00010,
+    B00010,
+    B00011};
 byte batteryCenterFull[] = {
-  B11111,
-  B00000,
-  B11110,
-  B11110,
-  B11110,
-  B11110,
-  B00000,
-  B11111
-};
+    B11111,
+    B00000,
+    B11110,
+    B11110,
+    B11110,
+    B11110,
+    B00000,
+    B11111};
 byte batteryCenterEmpty[] = {
-  B11111,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B00000,
-  B11111
-};
+    B11111,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B00000,
+    B11111};
 byte batteryRight[] = {
-  B10000,
-  B10000,
-  B11100,
-  B11100,
-  B11100,
-  B11100,
-  B10000,
-  B10000
-};
-
-
+    B10000,
+    B10000,
+    B11100,
+    B11100,
+    B11100,
+    B11100,
+    B10000,
+    B10000};
 
 // button states
-bool yellowButtonState = true; // false means pressed
+bool yellowButtonState = true; // true means not pressed, false means pressed
 bool blueButtonState = true;
 bool greenButtonState = true;
 bool redButtonState = true;
 
-
-// modes
+// modes (0: random number, 1: geiger counter, 2: battery voltage)
 short mode = 0;
 short nModes = 3;
-
 
 // mode 0: random number
 bool interrupted = false;
@@ -126,7 +111,6 @@ short randomRange = 6;
 short randomNumber = 1;
 char randomNumberChars[4];
 
-
 // mode 1: geiger counter
 short count = 0;
 unsigned long lastUpdate = 0;
@@ -135,31 +119,28 @@ char countsPerMinuteChars[7];
 float sievert = 0.0;
 char sievertChars[6];
 
-
 // mode 2: battery voltage
 float batteryVoltage = 0;
 char batteryVoltageChars[10];
 short batterySymbolState;
 
-
 // will be called on interrupt by particle detection
-// do not write to the LCD in this function !!! (loop will break)
-void interrupt() {
-  randomNumber = (micros() / MICROS_FAKTOR) % randomRange + 1;
+// do not write to the LCD in this function!!! (loop will break)
+void interrupt()
+{
+  randomNumber = (micros() / MICROS_FAKTOR) % randomRange + 1; // get new random number using division with remainder
   Serial.println("interrupt");
   interrupted = true;
-  count++;
+  count++; // increase counts per minutes
   digitalWrite(SIGNAL_LAMP, HIGH);
 }
 
-
-void setup() {
-
+void setup()
+{
 
   // serial
   Serial.begin(9600);
   Serial.println("starting");
-
 
   // pins
   pinMode(GEIGER_COUNTER, INPUT);
@@ -170,8 +151,7 @@ void setup() {
   pinMode(SIGNAL_LAMP, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(GEIGER_COUNTER), interrupt, FALLING);
 
-
-  // start lcd
+  // start LCD display
   lcd.init();
   lcd.createChar(0, radioactiveUpperLeft);
   lcd.createChar(1, radioactiveUpperCenter);
@@ -193,48 +173,53 @@ void setup() {
   lcd.print("              ");
 }
 
+void loop()
+{
 
-void loop() {
-
-
-  // yellow button
-  if(yellowButtonState && ! digitalRead(YELLOW_BUTTON)){
+  // yellow button / change mode
+  if (yellowButtonState && !digitalRead(YELLOW_BUTTON)) // is pressed but wasn't before
+  {
     Serial.println("yellow button");
     mode = (mode + 1) % nModes;
     Serial.println(mode);
-    if(mode == 0) {
+    if (mode == 0) // random number
+    {
       sprintf(randomNumberChars, "%03d", randomNumber);
-      lcd.setCursor(4,0);
+      lcd.setCursor(4, 0);
       lcd.print("R(");
-      lcd.print((char) (randomRangeDigits[2] + 48));
-      lcd.print((char) (randomRangeDigits[1] + 48));
-      lcd.print((char) (randomRangeDigits[0] + 48));
+      lcd.print((char)(randomRangeDigits[2] + 48)); // digit as char
+      lcd.print((char)(randomRangeDigits[1] + 48)); // digit as char
+      lcd.print((char)(randomRangeDigits[0] + 48)); // digit as char
       lcd.print(")=  ");
       lcd.print(randomNumberChars);
-      lcd.setCursor(4,1);
+      lcd.setCursor(4, 1);
       lcd.print("            ");
-    } else if(mode == 1) {
+    }
+    else if (mode == 1) // geiger counter
+    {
       waitForRandomNumber = false;
       digit = 0;
       dtostrf(countsPerMinute, 6, 1, countsPerMinuteChars);
       dtostrf(sievert, 5, 2, sievertChars);
-      lcd.setCursor(4,0);
+      lcd.setCursor(4, 0);
       lcd.print("A=");
       lcd.print(countsPerMinuteChars);
       lcd.print("/min");
-      lcd.setCursor(4,1);
+      lcd.setCursor(4, 1);
       lcd.print("E=");
       lcd.print(sievertChars);
       lcd.print("\xE4Sv/h");
-    } else if(mode == 2) {
+    }
+    else if (mode == 2) // battery voltage
+    {
       waitForRandomNumber = false;
       digit = 0;
       dtostrf(batteryVoltage, 9, 2, batteryVoltageChars);
-      lcd.setCursor(4,0);
+      lcd.setCursor(4, 0);
       lcd.print("U=");
       lcd.print(batteryVoltageChars);
       lcd.print("V");
-      lcd.setCursor(4,1);
+      lcd.setCursor(4, 1);
       lcd.print("      ");
       lcd.write(byte(4));
       lcd.write(byte(5));
@@ -242,70 +227,77 @@ void loop() {
       lcd.write(byte(6));
       lcd.write(byte(6));
       lcd.write(byte(7));
-     }
+    }
   }
   yellowButtonState = digitalRead(YELLOW_BUTTON);
 
-
-  //blue button
-  if(blueButtonState && ! digitalRead(BLUE_BUTTON)) {
+  // blue button / change selected digit of randomNumberRange
+  if (blueButtonState && !digitalRead(BLUE_BUTTON)) // is pressed but wasn't before
+  {
     Serial.print("blue button");
-    if(digit && (mode == 0)) {
-      randomRangeDigits[digit - 1] = (randomRangeDigits[digit-1] + 1) % 10;
+    if (digit && (mode == 0))
+    {
+      randomRangeDigits[digit - 1] = (randomRangeDigits[digit - 1] + 1) % 10;
       randomRange = randomRangeDigits[0] + 10 * randomRangeDigits[1] + 100 * randomRangeDigits[2];
       lcd.setCursor(9 - digit, 0);
-      lcd.print((char) (randomRangeDigits[digit - 1] + 48));
+      lcd.print((char)(randomRangeDigits[digit - 1] + 48)); // digit as char
     }
-    Serial.println((char) (randomRangeDigits[digit - 1] + 48));
+    Serial.println((char)(randomRangeDigits[digit - 1] + 48)); // digit as char
   }
   blueButtonState = digitalRead(BLUE_BUTTON);
 
-
-  // green button
-  if(greenButtonState && ! digitalRead(GREEN_BUTTON)) {
+  // green button / select a digit of randomNumberRange
+  if (greenButtonState && !digitalRead(GREEN_BUTTON))
+  {
     Serial.print("green button");
-    if(mode == 0) {
+    if (mode == 0)
+    {
       digit = (digit + 1) % 4;
-      if(digit) {
+      if (digit)
+      {
         lcd.setCursor(9 - digit, 1);
-        lcd.print("^");
-        lcd.print(" ");
+        lcd.print("^"); // print cursor
+        lcd.print(" "); // overwrite old cursor
       }
-      else {
+      else
+      {
         lcd.setCursor(6, 1);
-        lcd.print("   ");
+        lcd.print("   "); // overwrite old cursor
       }
     }
     Serial.println(digit);
   }
   greenButtonState = digitalRead(GREEN_BUTTON);
 
-
-  // red button
-  if(redButtonState && ! digitalRead(RED_BUTTON)) {
+  // red button / get new random number
+  if (redButtonState && !digitalRead(RED_BUTTON)) // is pressed but wasn't before
+  {
     Serial.println("red button");
     digitalWrite(SIGNAL_LAMP, LOW);
-    if(mode == 0) {
+    if (mode == 0)
+    {
       lcd.setCursor(13, 0);
       lcd.print(" ? ");
-      delay(300); // better visual feedback
+      delay(300); // for better visual feedback
     }
 
-    waitForRandomNumber = true;
+    waitForRandomNumber = true; // new number will now be generated by "radioactive interrupt"
   }
   redButtonState = digitalRead(RED_BUTTON);
 
-
   // update lcd
-  if(mode == 0 && interrupted && waitForRandomNumber) { // update mode 1: dice
+  if (mode == 0 && interrupted && waitForRandomNumber)
+  { // update mode 0: random number
     sprintf(randomNumberChars, "%03d", randomNumber);
     lcd.setCursor(13, 0);
     lcd.print(randomNumberChars);
     interrupted = false;
     waitForRandomNumber = false;
-  }else if(mode == 1 && ((millis() - lastUpdate) > COUNTING_INTERVAL)) { // update mode 1: geiger counter
+  }
+  else if (mode == 1 && ((millis() - lastUpdate) > COUNTING_INTERVAL))
+  { // update mode 1: geiger counter
     countsPerMinute = count * (60000.0 / COUNTING_INTERVAL) - BACKGROUND;
-    sievert = countsPerMinute/CONVERSION_INDEX;
+    sievert = countsPerMinute / CONVERSION_INDEX;
     dtostrf(countsPerMinute, 6, 1, countsPerMinuteChars);
     dtostrf(sievert, 5, 2, sievertChars);
     lcd.setCursor(6, 0);
@@ -316,7 +308,9 @@ void loop() {
     lcd.print("\xE4Sv/h");
     lastUpdate = millis();
     count = 0;
-  } else if(mode == 2) { // update mode 2: battery voltage
+  }
+  else if (mode == 2)
+  { // update mode 2: battery voltage
     batteryVoltage = analogRead(VOLTAGE) * VOLTAGE_SCALE;
     dtostrf(batteryVoltage, 9, 2, batteryVoltageChars);
     batterySymbolState = map(batteryVoltage, 4.5, 6.1, 0, 4);
@@ -324,15 +318,18 @@ void loop() {
     lcd.print(batteryVoltageChars);
     lcd.print("V");
     lcd.setCursor(11, 1);
-    for (short i=0; i<4; i++) {
-      if(i<=batterySymbolState) {
+    for (short i = 0; i < 4; i++)
+    {
+      if (i <= batterySymbolState)
+      {
         lcd.write(byte(5));
-      } else {
+      }
+      else
+      {
         lcd.write(byte(6));
       }
     }
   }
-
 
   // delay
   delay(100);
